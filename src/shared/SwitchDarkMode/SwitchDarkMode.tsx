a0c5779414421f0c60c709056f0a1ace87fa9e0c
Mauricio Henao <atomauro@cosmos-2.local>
2023-06-14 01:30:39 -0500

    .

--- src/shared/SwitchDarkMode/SwitchDarkMode.tsx
+++ src/shared/SwitchDarkMode/SwitchDarkMode.tsx
@@ -46,13 +46,14 @@ const SwitchDarkMode: React.FC<SwitchDarkModeProps> = ({ className = "" }) => {
   return (
     <button
       onClick={_toogleDarkMode}
-      className={`text-2xl md:text-3xl w-12 h-12 rounded-full text-neutral-700 dark:text-neutral-300 hover:bg-neutral-100 dark:hover:bg-neutral-800 focus:outline-none flex items-center justify-center ${className}`}
+      className={` text-2xl md:text-3xl w-12 h-12 rounded-full text-neutral-700 dark:text-neutral-300 hover:bg-neutral-100 dark:hover:bg-neutral-800 focus:outline-none flex items-center justify-center ${className}`}
+      style={{ display: "none" }}
     >
       <span className="sr-only">Enable dark mode</span>
       {isDarkMode ? (
-        <MoonIcon className="w-7 h-7" aria-hidden="true" />
+        <MoonIcon className="w-7 h-7 hide" aria-hidden="true" />
       ) : (
-        <SunIcon className="w-7 h-7" aria-hidden="true" />
+        <SunIcon className="w-7 h-7 hide" aria-hidden="true" />
       )}
     </button>
   );
